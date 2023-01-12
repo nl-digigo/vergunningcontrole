3a2186a0dd274a11d4ebba9864e3a333529a7a0e
Elisabeth de Vries <elisabeth.kloren@crow.nl>
2023-01-12 09:19:17 +0100

    update vergunningcontrole

---
+++ docs/usecase/config.js
@@ -0,0 +1,16 @@
+var respecConfig = {
+  // => https://github.com/stichting-crow/respec/wiki
+  specStatus: "WV",
+  shortName: "usecase",
+  specType: "Handreiking"
+  imprint: "bimloket",
+  editors: [
+    // alleen `name` is verplicht, https://respec.org/docs/#person
+    {
+      name: "Rolf Jonker",
+      company: "Rotterdam",
+    },
+  ],
+ github: "https://github.com/bimloket/vergunningcontrole",
+ license: "cc-by",
+};
---
+++ docs/usecase/h/0-samenvatting.md
@@ -0,0 +1,4 @@
+# Samenvatting
+
+
+kjnpgjbgbn

c189ae2944ad1041607604d5a37e098239e6fc76
SissiELy <SissiELy@users.noreply.github.com>
2026-08-19 11:30:07 +0000

    doc: add toolkit-geobim
    Reason: run add-doc.yaml by SissiELy

---
+++ docs/toolkit-geobim/config.js
@@ -0,0 +1,15 @@
+var respecConfig = {
+  // => https://github.com/stichting-crow/respec/wiki
+  specStatus: "WD",
+  shortName: "vergunningcontrole/toolkit-geobim",
+  specType: "technische documentatie", // vrije tekst, kleine letter
+  imprint: "crow",
+  editors: [
+    // alleen `name` is verplicht, https://respec.org/docs/#person
+    {
+      name: "Siham El Yamani",
+    },
+  ],
+  github: "https://github.com/Stichting-CROW/vergunningcontrole",
+  license: "undefined",
+};
